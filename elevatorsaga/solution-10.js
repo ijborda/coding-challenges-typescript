ce5aa6f3b786b86e6fec43a6cd0eaaf889efacbd
Imari Joy Borda <37444801+ijborda@users.noreply.github.com>
2025-04-06 17:56:11 +0800

    mod sol 10

--- elevatorsaga/solution-10.js
+++ elevatorsaga/solution-10.js
@@ -1,7 +1,5 @@
 const fn = {
   init: function (elevators, floors) {
-    console.log(elevators);
-
     const getNearestFloor = (currentFloor, targetFloors) => {
       const distances = targetFloors.map(floor => Math.abs(floor - currentFloor));
       const minDistance = Math.min(...distances);
@@ -43,76 +41,31 @@ const fn = {
       return elevator.getPressedFloors().includes(targetFloorNum);
     };
 
-    const queueFloor = (elevator, floorNum) => {
-      elevator.goToFloor(floorNum);
-      rearrangeQueue(elevator);
-    };
-
-    const sortQueue = (arr) => {
-      let array = [...arr.map(item => Number(item))];
-      let batchNum = 0;
-      const sorted = [];
-      while (array.length !== 0) {
-        const batch = Array.from(new Set(array)).sort((a, b) => a - b);
-        if (batchNum % 2 === 0) {
-          sorted.push(...batch);
-        } else {
-          sorted.push(...batch.reverse());
-        }
-        array = removeFirstMatchesInArray(array, batch);
-        batchNum += 1;
-      }
-      return sorted;
-    };
-
-    const removeFirstMatchInArray = (array, item) => {
-      const arrayCopy = [...array];
-      const index = arrayCopy.indexOf(item);
-      if (index > -1) { // only splice array when item is found
-        arrayCopy.splice(index, 1); // 2nd parameter means remove one item only
-      }
-      return arrayCopy;
-    };
-
-    const removeFirstMatchesInArray = (array, items) => {
-      let arrayCopy = [...array];
-      items.forEach(item => {
-        arrayCopy = removeFirstMatchInArray(arrayCopy, item);
-      });
-      return arrayCopy;
-    };
-
-    const rearrangeQueue = (elevator) => {
-      elevator.destinationQueue = sortQueue(elevator.destinationQueue);
-      elevator.checkDestinationQueue();
-    };
-
     // eslint-disable-next-line array-callback-return
     elevators.map((elevator, i) => {
-      elevator.on('floor_button_pressed', function (floorNum) {
-        queueFloor(elevator, floorNum);
+      elevator.on('floor_button_pressed', function () {
+        // elevator.goToFloor(floorNum);
       });
 
-      elevator.on('stopped_at_floor', function () {
+      elevator.on('stopped_at_floor', function (floorNum) {
         if (isElevatorFull(elevator)) {
-          const target = getNearestFloor(elevator.currentFloor(), elevator.getPressedFloors());
-          elevator.destinationQueue = [target, ...elevator.destinationQueue];
-          elevator.checkDestinationQueue();
-          console.log('>>> stopped', i, target, elevator.destinationQueue);
+          const target = getNearestFloor(floorNum, elevator.getPressedFloors());
+          elevator.goToFloor(target, true);
           return;
         }
         if (elevator.destinationQueue.length > 0) {
           const nextFloorNumber = elevator.destinationQueue[0];
           if (isFloorEmpty(nextFloorNumber) && !isThereToUnload(nextFloorNumber, elevator)) {
-            console.log('>>> skipped floor', i, nextFloorNumber);
             elevator.destinationQueue = elevator.destinationQueue.slice(1);
             elevator.checkDestinationQueue();
           }
+          // eslint-disable-next-line no-useless-return
+          return;
         }
       });
 
       elevator.on('idle', function () {
-        queueFloor(elevator, 0);
+        elevator.goToFloor(0);
       });
     });
 
@@ -120,12 +73,12 @@ const fn = {
     floors.map(floor => {
       floor.on('up_button_pressed', function () {
         const elevator = getOptimalElevator(floor.level, elevators);
-        queueFloor(elevator, floor.level);
+        elevator.goToFloor(floor.level);
       });
 
       floor.on('down_button_pressed', function () {
         const elevator = getOptimalElevator(floor.level, elevators);
-        queueFloor(elevator, floor.level);
+        elevator.goToFloor(floor.level);
       });
     });
   },
